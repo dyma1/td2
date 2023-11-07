f1fc7ffc99261226407fa8bb3d516144d00f5c08
Victor <victor.gasmi@etu.umontpellier.fr>
2023-11-07 17:18:22 +0100

    fonction executer
    fonction executer terminée
    
    fonction executer terminée
    
    fonction executer terminée
    
    (cherry picked from commit 906b9d28f432f9d5b8cd907f74474f9d895c03fa)

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,24 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument{
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer(){
+        if(parameters.length<3){
+            System.err.println("Format attendu : remplacer;positionDebut;positionFin;texte");
+            return;
+        }
+        int debut = Integer.parseInt(parameters[1]);
+        int fin = Integer.parseInt(parameters[2]);
+        System.out.println(parameters.length);
+        if(parameters.length<4) this.document.remplacer(debut, fin, "");
+        else this.document.remplacer(debut, fin, parameters[3]);
+        super.executer();
+    }
+}
